5a36c0ff3dd27c728336f52abc73dca3628dc0c8
litsky101 <desagunangelito@gmail.com>
2023-10-08 22:14:16 +0800

    Add web api consume

---
+++ NZWalks.WebApp/Controllers/RegionController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.WebApp.Controllers
+{
+    public class RegionController : Controller
+    {
+        private readonly IHttpClientFactory clientFactor;
+
+        public RegionController(IHttpClientFactory _clientFactor)
+        {
+            clientFactor = _clientFactor;
+        }
+        public async Task<IActionResult> Index()
+        {
+            try
+            {
+                var client = clientFactor.CreateClient();
+
+                var httpResponse = await client.GetAsync("https://localhost:7220/api/regions");
+
+                httpResponse.EnsureSuccessStatusCode();
+
+                var responseBody = await httpResponse.Content.ReadAsStringAsync();
+
+                ViewBag.Response = responseBody;
+            }
+            catch (Exception ex)
+            {
+
+                
+            }
+
+            return View();
+        }
+    }
+}
